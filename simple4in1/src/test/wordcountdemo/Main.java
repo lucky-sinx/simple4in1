5faf81f7e58a3486dd831140a9fada4912cf8271
ZeroHou <769617775@qq.com>
2021-10-15 17:19:23 +0800

    update

--- simple4in1/src/test/wordcountdemo/Main.java
+++ simple4in1/src/test/wordcountdemo/Main.java
@@ -1,7 +1,14 @@
 package test.wordcountdemo;
 
+import com.zju.fourinone.Context;
+import com.zju.fourinone.LocalFileSystem;
+
+import java.rmi.RemoteException;
+
 public class Main {
-    public static void main(String[] args)  {
-//        Context.startPark();
+    public static void main(String[] args) throws RemoteException {
+        Context.startContext();
+        LocalFileSystem fileSystem = Context.getFileSystem();
+        System.out.println(fileSystem.exists(""));
     }
 }
